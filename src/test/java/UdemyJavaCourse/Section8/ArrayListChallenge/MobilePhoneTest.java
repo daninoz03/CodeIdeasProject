425579f5211805175753f2e502a6df7e24bc3063
daninoz03 <daninoz03@yahoo.com>
2019-03-18 21:31:36 +0000

    Adding array tests (#5)

--- src/test/java/UdemyJavaCourse/Section8/ArrayListChallenge/MobilePhoneTest.java
+++ src/test/java/UdemyJavaCourse/Section8/ArrayListChallenge/MobilePhoneTest.java
@@ -3,20 +3,24 @@ package UdemyJavaCourse.Section8.ArrayListChallenge;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.ArrayList;
 
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.is;
 
+@RunWith(MockitoJUnitRunner.class)
 public class MobilePhoneTest {
 
     private ArrayList<Contact> contacts = new ArrayList<>();
 
-    private  MobilePhone mobilePhone = new MobilePhone();
+    private MobilePhone mobilePhone = new MobilePhone();
 
     @Before
     public void setUp() {
-        contacts.add(new Contact("Dan","12345"));
+        contacts.add(new Contact("Dan", "12345"));
         contacts.add(new Contact("John", "087212"));
         contacts.add(new Contact("Sarah", "086543"));
         mobilePhone.setMyContacts(contacts);
@@ -34,7 +38,7 @@ public class MobilePhoneTest {
     }
 
     @Test
-    public void shouldReturnCorrectIndexAfterAddingContact(){
+    public void shouldReturnCorrectIndexAfterAddingContact() {
         // given
         Contact contact = new Contact("Ally", "085123");
         // when
@@ -43,5 +47,27 @@ public class MobilePhoneTest {
         // then
         Assert.assertThat(contactWasAdded, is(true));
         Assert.assertThat(mobilePhone.findContact("Ally"), is(3));
+    }
+
+    @Test
+    public void shouldReturnCorrectIndexAfterUpdatingContact() {
+        // given
+        Contact contact = new Contact("DanNew", "54321");
+        // when
+        mobilePhone.updateContact("Dan", contact);
+
+        // then
+        Assert.assertThat(mobilePhone.findContact("DanNew"), greaterThan(-1));
+    }
+
+    @Test
+    public void shouldRemoveContact() {
+        // given
+
+        // when
+        mobilePhone.removeContact("Dan");
+
+        // then
+        Assert.assertThat(mobilePhone.findContact("Dan"), is(-1));
     }
 }
